daa6768c68aa0df6d1fc6f861410b960a6dcf046
Ivanildo Dos Santos Queiroz <isqueiroz@br-atacadao.corp>
2024-05-03 16:21:44 -0300

    get movies api

--- js/API.js
+++ js/API.js
@@ -17,10 +17,15 @@ function getMoviesApi(mov) {
       const li = document.createElement("li");
       let title = document.createElement("h2");
       title.innerHTML = mov[i].title;
-      let img = document.createElement('img');
-      img.setAttribute('src',`https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`);
+      let img = document.createElement("img");
+      img.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
+      );
       let p1 = document.createElement("p");
-      if(mov[i].original_language == 'en'){p1.innerHTML = 'English'};
+      if (mov[i].original_language == "en") {
+        p1.innerHTML = "English";
+      }
       let p2 = document.createElement("p");
       p2.innerHTML = mov[i].overview;
       li.appendChild(title);
@@ -32,41 +37,74 @@ function getMoviesApi(mov) {
       movies.appendChild(li);
     }
   }
-  function getAllMoviesTheme(mov){
-    for (let i = 0; i <= 2; i++) {
-      const li = document.createElement("li");
-      let title = document.createElement("h2");
-      title.innerHTML = mov[i].title;
-      let img = document.createElement('img');
-      img.setAttribute('src',`https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`);
-      let p1 = document.createElement("p");
-      if(mov[i].original_language == 'en'){p1.innerHTML = 'English'};
-      let p2 = document.createElement("p");
-      p2.innerHTML = mov[i].overview;
-      li.appendChild(title);
-      li.appendChild(img);
-      li.appendChild(p1);
-      li.appendChild(p2);
-      //li.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`;
-      li.classList.add("active-movies");
-      movies.appendChild(li);
+  function getAllMoviesTheme(mov) {
+    const listAction = document.querySelector(".action");
+    const listTerror = document.querySelector(".terror");
+    const listLanc = document.querySelector(".lanc");
+
+    for (let i = 0; i < mov.length; i++) {
+      const li_01 = document.createElement("li");
+      let img = document.createElement("img");
+      img.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
+      );
+      let p = document.createElement("p");
+      p.innerHTML = mov[i].title;
+
+      li_01.appendChild(img);
+      li_01.appendChild(p);
+      listAction.appendChild(li_01);
+      
     }
-    console.log(mov)
+    for (let i = 0; i < mov.length; i++) {
+      
+      const li_02 = document.createElement("li");
+      let img = document.createElement("img");
+      img.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
+      );
+      let p = document.createElement("p");
+      p.innerHTML = mov[i].title;
+
+      
+      li_02.appendChild(img);
+      li_02.appendChild(p);
+      listLanc.appendChild(li_02);
+     
+    }
+    for (let i = 0; i < mov.length; i++) {
+      
+      const li_03 = document.createElement("li");
+      let img = document.createElement("img");
+      img.setAttribute(
+        "src",
+        `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
+      );
+      let p = document.createElement("p");
+      p.innerHTML = mov[i].title;
+
+      
+      li_03.appendChild(img);
+      li_03.appendChild(p);
+      listTerror.appendChild(li_03)
+    }
   }
-  getAllMoviesTheme(mov)
+  getAllMoviesTheme(mov);
   cardsMoviesInitial(mov);
 }
-function actionNavbar(){
-  const navBar = document.querySelector('.navbar');
-  const btnClose = document.querySelector('#iconClose');
-  const butnOpen = document.querySelector('#menuNavbar');
 
-  btnClose.addEventListener('click',()=>{
-    navBar.classList.toggle('active-navbar');
-  })
-  butnOpen.addEventListener('click',()=>{
-    navBar.classList.add('active-navbar');
-   
-  })
+function actionNavbar() {
+  const navBar = document.querySelector(".navbar");
+  const btnClose = document.querySelector("#iconClose");
+  const butnOpen = document.querySelector("#menuNavbar");
+
+  btnClose.addEventListener("click", () => {
+    navBar.classList.toggle("active-navbar");
+  });
+  butnOpen.addEventListener("click", () => {
+    navBar.classList.add("active-navbar");
+  });
 }
 actionNavbar();
