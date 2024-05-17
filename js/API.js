3476c864a63638c7cae79d13d071e6176abcfee8
Ivanildo Dos Santos Queiroz <isqueiroz@br-atacadao.corp>
2024-05-17 10:16:37 -0300

    carosel separate of lists

--- js/API.js
+++ js/API.js
@@ -75,7 +75,6 @@ function getMoviesApi(mov, ser) {
     const allArrays = [ArraylistAction,ArraylistLanc,ArraylistTerror];
 
     
-    
     for (let i = 0; i < mov.length; i++) {
       const li_01 = document.createElement("li");
       let img = document.createElement("img");
@@ -131,25 +130,59 @@ function getMoviesApi(mov, ser) {
     })
     
     function activeBtn() {
-      let current_item = 0;
+      let current_item_lanc = 0;
+      let current_item_action = 0;
+      let current_item_terror = 0;
       const btn = document.querySelectorAll(".btn");   
+
       btn.forEach((btn) => {
         btn.addEventListener("click", (e) => {
          btn_current = (e.target.parentNode).parentNode.classList.value
           console.log(btn_current)
           const left = e.target.classList.contains("left");
+
           if (left && btn_current == 'action') {
-            console.log(left)
-            current_item = (current_item > 0) ? current_item - 1 : 0;
+            current_item_action = (current_item_action > 0) ? current_item_action - 1 : 0;
           } else if(!left && btn_current == 'action'){
-            current_item = (current_item < ArraylistAction.length - 1) ? current_item + 1 : ArraylistAction.length - 1;
+            current_item_action = (current_item_action < ArraylistAction.length - 1) ? current_item_action + 1 : ArraylistAction.length - 1;
           }
+          console.log(current_item_action)
+
+          if (left && btn_current == 'lanc') {
+            console.log(left)
+            current_item_lanc = (current_item_lanc > 0) ? current_item_lanc - 1 : 0;
+          } else if(!left && btn_current == 'lanc'){
+            current_item_lanc = (current_item_lanc < ArraylistLanc.length - 1) ? current_item_lanc + 1 : ArraylistAction.length - 1;
+          }
+
+          if (left && btn_current == 'terror') {
+            console.log(left)
+            current_item_terror = (current_item_terror > 0) ? current_item_terror - 1 : 0;
+          } else if(!left && btn_current == 'terror'){
+            current_item_terror = (current_item_terror < ArraylistTerror.length - 1) ? current_item_terror + 1 : ArraylistAction.length - 1;
+          }
+
           updateCurrentItem();
         });
       });
     
+      ArraylistLanc.forEach((item, i) => {
+        item.addEventListener('click', () => {
+          console.log(item.parentNode)
+          current_item = i;
+          updateCurrentItem();
+        });
+      });
+      ArraylistTerror.forEach((item, i) => {
+        item.addEventListener('click', () => {
+          console.log(item.parentNode)
+          current_item = i;
+          updateCurrentItem();
+        });
+      });
       ArraylistAction.forEach((item, i) => {
         item.addEventListener('click', () => {
+          console.log(item.parentNode)
           current_item = i;
           updateCurrentItem();
         });
@@ -157,7 +190,31 @@ function getMoviesApi(mov, ser) {
     
       function updateCurrentItem() {
         ArraylistAction.forEach((item, index) => {
-          if (index === current_item) {
+          if (index === current_item_action) {
+            item.classList.add('current-item');
+            item.scrollIntoView({
+              behavior: "smooth",
+              inline: "center",
+              block: "nearest",
+            });
+          } else {
+            item.classList.remove('current-item');
+          }
+        });
+        ArraylistLanc.forEach((item, index) => {
+          if (index === current_item_lanc) {
+            item.classList.add('current-item');
+            item.scrollIntoView({
+              behavior: "smooth",
+              inline: "center",
+              block: "nearest",
+            });
+          } else {
+            item.classList.remove('current-item');
+          }
+        });
+        ArraylistTerror.forEach((item, index) => {
+          if (index === current_item_terror) {
             item.classList.add('current-item');
             item.scrollIntoView({
               behavior: "smooth",
