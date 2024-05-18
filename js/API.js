776b24e98477650bfa141f195e55e4f651c1e184
Ivanildo Dos Santos Queiroz <isqueiroz@br-atacadao.corp>
2024-05-18 15:45:45 -0300

    run menu suspense

--- js/API.js
+++ js/API.js
@@ -1,13 +1,14 @@
 const url_movie =
   "https://api.themoviedb.org/3/discover/movie?api_key=a406b143ca1f900e34b5f94b65620223";
 const url_series =
-  "https://api.themoviedb.org/3/discover/tv?api_key=a406b143ca1f900e34b5f94b65620223";
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=a406b143ca1f900e34b5f94b65620223";
 const movies = document.querySelector(".list-movies");
 function getApi(url_movie, url_series) {
   const seriePromise = fetch(url_series)
     .then((res) => res.json())
     .then((data) => {
-      return data.results;
+      return data.results
+      
     });
   const moviePromise = fetch(url_movie)
     .then((res) => res.json())
@@ -27,6 +28,7 @@ getApi(url_movie, url_series);
 function getMoviesApi(mov, ser) {
   function cardsMoviesInitial(mov, ser) {
     for (let i = 0; i <= 2; i++) {
+
       const li = document.createElement("li");
       let title = document.createElement("h2");
       title.innerHTML = mov[i].title;
@@ -36,9 +38,11 @@ function getMoviesApi(mov, ser) {
         `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
       );
       let p1 = document.createElement("p");
+
       if (mov[i].original_language == "en") {
         p1.innerHTML = "English";
       }
+
       let p2 = document.createElement("p");
       p2.innerHTML = mov[i].overview;
       li.appendChild(title);
@@ -47,22 +51,22 @@ function getMoviesApi(mov, ser) {
       li.classList.add("active-movies");
       movies.appendChild(li);
     }
-    function getLists() {
-      const allLits = document.querySelector(".list-movies");
-      const lists = allLits.querySelectorAll("li");
-      let i = 0;
-      setInterval(() => {
-        if (i >= lists.length) {
-          i = 0;
-        }
-        lists[i].scrollIntoView({
-          behavior: "smooth",
-          inline: "start",
-          block: "nearest",
-        });
-        i++;
-      }, 1000);
-    }
+    // function getLists() {
+    //   const allLits = document.querySelector(".list-movies");
+    //   const lists = allLits.querySelectorAll("li");
+    //   let i = 0;
+    //   setInterval(() => {
+    //     if (i >= lists.length) {
+    //       i = 0;
+    //     }
+    //     lists[i].scrollIntoView({
+    //       behavior: "smooth",
+    //       inline: "start",
+    //       block: "nearest",
+    //     });
+    //     i++;
+    //   }, 1000);
+    // }
     //getLists();
   }
   function getAllMoviesTheme(mov) {
@@ -76,59 +80,50 @@ function getMoviesApi(mov, ser) {
 
     
     for (let i = 0; i < mov.length; i++) {
-      const li_01 = document.createElement("li");
-      let img = document.createElement("img");
-      img.setAttribute(
-        "src",
-        `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
-      );
-      let p = document.createElement("p");
-      p.innerHTML = mov[i].title;
+      let [movie,allData] = createElementsList(mov[i])
+      //console.log(movie)
+      ArraylistLanc.push(movie)
+      listLanc.appendChild(ArraylistLanc[i]);
+
+    }
+    
+    for (let i = 0; i < mov.length; i++) {
+      let [movie,allData] = createElementsList(mov[i])
+      ArraylistAction.push(movie)
+      listAction.appendChild(ArraylistAction[i]);
       
-      li_01.appendChild(img);
-      li_01.appendChild(p);
-      listAction.appendChild(li_01);
-      ArraylistAction.push(li_01)
     }
-    for (let i = 0; i < mov.length; i++) {
-      const li_02 = document.createElement("li");
+    for (let i = 0; i < ser.length; i++) {
+      let [movie,allData] = createElementsList(mov[i])
+      ArraylistTerror.push([movie,allData])
+      listTerror.appendChild(ArraylistTerror[i][0]);
+
+    }
+    
+    // allArrays.map((e,i,a)=>{
+    //  allArrays[i].map(e=>{
+    //   e.addEventListener('click',()=>{
+    //     // verificação para o botao console.log(e.parentNode.classList.contains('lanc'))
+    //     })
+    //  })
+    // });
+
+    function createElementsList(data){
+      const li = document.createElement("li");
       let img = document.createElement("img");
       img.setAttribute(
         "src",
-        `https://image.tmdb.org/t/p/w500/${mov[i].poster_path})`
+        `https://image.tmdb.org/t/p/w500/${data.poster_path})`
       );
       let p = document.createElement("p");
-      p.innerHTML = mov[i].title;
+      p.innerHTML = data.title;
+      li.appendChild(img);
+      li.appendChild(p);
 
-      li_02.appendChild(img);
-      li_02.appendChild(p);
-      listLanc.appendChild(li_02);
-      ArraylistLanc.push(li_02)
-    }
-    for (let i = 0; i < mov.length; i++) {
-      const li_03 = document.createElement("li");
-      let img = document.createElement("img");
-      img.setAttribute(
-        "src",
-        `https://image.tmdb.org/t/p/w500/${ser[i].poster_path})`
-      );
-      let p = document.createElement("p");
-      p.innerHTML = mov[i].title;
+      return [li,data];
 
-      li_03.appendChild(img);
-      li_03.appendChild(p);
-      listTerror.appendChild(li_03);
-      ArraylistTerror.push(li_03)
     }
-    
-    allArrays.map((e,i,a)=>{
-     allArrays[i].map(e=>{
-      e.addEventListener('click',()=>{
-        // verificação para o botao console.log(e.parentNode.classList.contains('lanc'))
-        })
-     })
-    })
-    
+
     function activeBtn() {
       let current_item_lanc = 0;
       let current_item_action = 0;
@@ -138,7 +133,6 @@ function getMoviesApi(mov, ser) {
       btn.forEach((btn) => {
         btn.addEventListener("click", (e) => {
          btn_current = (e.target.parentNode).parentNode.classList.value
-          console.log(btn_current)
           const left = e.target.classList.contains("left");
 
           if (left && btn_current == 'action') {
@@ -146,17 +140,14 @@ function getMoviesApi(mov, ser) {
           } else if(!left && btn_current == 'action'){
             current_item_action = (current_item_action < ArraylistAction.length - 1) ? current_item_action + 1 : ArraylistAction.length - 1;
           }
-          console.log(current_item_action)
 
           if (left && btn_current == 'lanc') {
-            console.log(left)
             current_item_lanc = (current_item_lanc > 0) ? current_item_lanc - 1 : 0;
           } else if(!left && btn_current == 'lanc'){
             current_item_lanc = (current_item_lanc < ArraylistLanc.length - 1) ? current_item_lanc + 1 : ArraylistAction.length - 1;
           }
 
           if (left && btn_current == 'terror') {
-            console.log(left)
             current_item_terror = (current_item_terror > 0) ? current_item_terror - 1 : 0;
           } else if(!left && btn_current == 'terror'){
             current_item_terror = (current_item_terror < ArraylistTerror.length - 1) ? current_item_terror + 1 : ArraylistAction.length - 1;
@@ -168,22 +159,20 @@ function getMoviesApi(mov, ser) {
     
       ArraylistLanc.forEach((item, i) => {
         item.addEventListener('click', () => {
-          showDataMovie()
-          console.log(item.parentNode)
+          
           current_item_lanc = i;
           updateCurrentItem();
         });
       });
       ArraylistTerror.forEach((item, i) => {
-        item.addEventListener('click', () => {
-          console.log(item.parentNode)
+        item[0].addEventListener('click', () => {
+          showDataMovie(item[1])
           current_item_terror = i;
           updateCurrentItem();
         });
       });
       ArraylistAction.forEach((item, i) => {
         item.addEventListener('click', () => {
-          console.log(item.parentNode)
           current_item_action = i;
           updateCurrentItem();
         });
@@ -216,14 +205,14 @@ function getMoviesApi(mov, ser) {
         });
         ArraylistTerror.forEach((item, index) => {
           if (index === current_item_terror) {
-            item.classList.add('current-item');
-            item.scrollIntoView({
+            item[0].classList.add('current-item');
+            item[0].scrollIntoView({
               behavior: "smooth",
               inline: "center",
               block: "nearest",
             });
           } else {
-            item.classList.remove('current-item');
+            item[0].classList.remove('current-item');
           }
         });
       }
@@ -252,18 +241,32 @@ function actionNavbar() {
 }
 actionNavbar();
 
-function showDataMovie(){
-  console.log('iniciou')
+function showDataMovie(movie){
+  console.log(movie)
   const screen = document.querySelector('body');
   const fullScreen = document.createElement('div');
   fullScreen.setAttribute('class','full-screen');
   const btnReturnFullScreen = document.createElement('button');
   btnReturnFullScreen.setAttribute('id','return-full-screen');
-  btnReturnFullScreen.textContent = 'voltar'
+  btnReturnFullScreen.textContent = 'X'
+  const image = document.createElement('img');
+  image.setAttribute('src',`https://image.tmdb.org/t/p/w500/${movie.poster_path})`)
+  const text = document.createElement('p');
+  text.innerHTML = `${movie.overview}`;
+  console.log(movie.overview)
+  const title = document.createElement('h1');
+  title.textContent = movie.title;
+
+  fullScreen.appendChild(interfaceShowMovies(image,title,text,btnReturnFullScreen))
   fullScreen.appendChild(btnReturnFullScreen);
+
   fullScreen.classList.add('active-full-screen');
-  screen.appendChild(fullScreen);
-  console.log('encerrou');
+  screen.appendChild(fullScreen).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+    
+
+  });
 
   btnReturnFullScreen.addEventListener('click',()=>{
     fullScreen.classList.remove('active-full-screen');
@@ -271,11 +274,27 @@ function showDataMovie(){
   })
 }
 
-function interfaceShowMovies(){
+function interfaceShowMovies(image,data_title,content,span){
+  const dataMovies = document.createElement('div');
+  dataMovies.setAttribute('id','data-movies')
+
   const informationLeft = document.createElement('div');
+  informationLeft.setAttribute('class','information-left');
+  informationLeft.appendChild(image)
+
   const informationRight = document.createElement('div');
-  const informationCenterTitle = document.createElement('h2');
-  const informationCenterDescription = document.createElement('p');
-  const informationCenterPoster = document.createElement('img');
+  informationRight.setAttribute('class','information-right');
+
+  const button = document.createElement('button')
+  informationRight.appendChild(data_title);
+  informationRight.appendChild(content);
+  informationRight.appendChild(span);
+  informationRight.appendChild(button);
+
+  //button.addEventListener()
+dataMovies.appendChild(informationLeft)
+dataMovies.appendChild(informationRight)
+
+return dataMovies
   
 }
